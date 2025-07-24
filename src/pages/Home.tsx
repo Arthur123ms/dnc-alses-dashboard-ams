248fb1787d0f4ebf75968b5a65d265910b99d5a5
Arthur123ms <arthur123souza2004@outlook.com>
2025-07-24 15:32:17 -0300

    Feat: Card Components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
-import { Header } from "@/components"
+import {CardComponents, Header } from "@/components"
+import { Container } from "@mui/material"
 
 function Home () {
     return(
         <>
             <Header />
+            <Container maxWidth='lg'>
+                <CardComponents>Card</CardComponents>
+            </Container>
 
         </>
     )
