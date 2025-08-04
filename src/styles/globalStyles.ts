b26ffd5d2822887c3e9eec7319d0f26b09e1b1a4
Arthur123ms <arthur123souza2004@outlook.com>
2025-08-04 16:07:45 -0300

    Feat: Home Page Request

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -20,7 +20,31 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme}>`
         margin-bottom: ${pxToRem(16)};
     }
 
-     .mb-2{
+    .mb-2{
         margin-bottom: ${pxToRem(32)};
+    }
+    
+    .skeleton-loading {
+        animation: skaletonLoading 2s infinite alternate
+    }
+
+    @keyframes skeletonLoading {
+        from {
+            background-color: ${(props) => props.theme.appSkeletonFrom};
+
+        }
+
+        to {
+            background-color: ${(props) => props.theme.appSkeletonTo};
+            
+        }
+    }
+
+    .skeleton-loading-mh-1{
+        min-height: ${pxToRem(175)}
+    }
+
+    .skeleton-loading-mh-2{
+        min-height: ${pxToRem(400)}
     }
 `
